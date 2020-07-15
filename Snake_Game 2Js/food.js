83d7310a64fd899170b4d011ee22746bf8e9bca7
yixiang08 <68189479+yixiang08@users.noreply.github.com>
2020-07-15 22:13:00 +0800

    Add files via upload

---
+++ Snake_Game 2Js/food.js
@@ -0,0 +1,28 @@
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+let food = getRndomFoodPosition()
+const EXPANSION_RATE = 1
+
+export function update() {
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE)
+        food = getRndomFoodPosition()
+    }
+}
+
+export function draw(gameBoard) {
+        const foodElement = document.createElement('div')
+        foodElement.style.gridRowStart = food.y
+        foodElement.style.gridColumnStart = food.x
+        foodElement.classList.add('food')
+        gameBoard.appendChild(foodElement)
+}
+
+function getRndomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake (newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
+}
